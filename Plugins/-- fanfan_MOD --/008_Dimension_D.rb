18b4ae862d59eb65023a139d534e8b44f75ce808
Fanfan <erilanfun@gmail.com>
2024-11-20 20:54:37 +0800

    update

--- Plugins/-- fanfan_MOD --/008_Dimension_D.rb
+++ Plugins/-- fanfan_MOD --/008_Dimension_D.rb
@@ -11,12 +11,13 @@ module DimensionD
         pbMessage(msg)
       else
         data = pbChoosePkmnFromListEX(_INTL("Which Pokémon would you like to retrieve?"), allowed)
+        return if !data[0]
         pbAddPokemon(data[0])
         pkmns.delete_at(data[1])
       end
 =begin
       data = pbChoosePkmnFromListEX(_INTL("Which Pokémon would you like to retrieve?"), pkmns)
-      return if !data
+      return if !data[0]
       if has_species?(data[0].species, data[0].form)
         pbMessage(_INTL("You can't retrieve this Pokémon!"))
       else
