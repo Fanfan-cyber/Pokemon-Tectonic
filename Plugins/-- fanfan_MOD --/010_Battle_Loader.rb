d3646778548d09ef73d07ff85516b81beb31f59a
Fanfan-cyber <erilanfun@gmail.com>
2024-11-21 15:07:17 +0800

    update

--- Plugins/-- fanfan_MOD --/010_Battle_Loader.rb
+++ Plugins/-- fanfan_MOD --/010_Battle_Loader.rb
@@ -1,66 +1,116 @@
 module BattleLoader
   BATTLE_LOADER_PATH = "Team Data"
-  BATTLE_LOADER_FILE = Dir.glob("Team Data/team_data_*.txt")
-  @@battle_loader = {}
+  @@battle_loader = []
 
   def self.load_data
     Dir.mkdir(BATTLE_LOADER_PATH) rescue nil
-    BATTLE_LOADER_FILE.each do |team|
-      encrypted_data = File.read(team)
-      battle_info = Marshal.restore(Zlib::Inflate.inflate(encrypted_data.unpack("m")[0]))
-      @@battle_loader[team[20...-4]] = [battle_info[0], battle_info[1]]
+    @@battle_loader.clear
+    teams = Dir.glob("#{BATTLE_LOADER_PATH}/*.txt")
+    teams.each do |info|
+      encrypted_data = File.read(info)
+      team_info = Marshal.restore(Zlib::Inflate.inflate(encrypted_data.unpack("m")[0]))
+      @@battle_loader.push(team_info) # [rule, name, team, unique_id]
+      @@battle_loader.sort_by! { |team_info| team_info[0] }
     end
   end
 
-  def self.save_data(rule)
-    @@battle_loader["#{$Trainer.name}_#{generate_unique_id}"] = [$Trainer.party, rule]
-    @@battle_loader.each do |trainer, team|
-      encrypted_data = [Zlib::Deflate.deflate(Marshal.dump(team))].pack("m")
-      File.open("Team Data/team_data_#{trainer}.txt", "wb") do |file|
-        file.write(encrypted_data)
+  def self.add_data(rule, name = "", team = nil)
+    name = $Trainer.name if name.empty?
+    unique_id = generate_unique_id
+    new_team = [rule, name, team || $Trainer.party, unique_id]
+    encrypted_data = [Zlib::Deflate.deflate(Marshal.dump(new_team))].pack("m")
+    File.open("Team Data/#{rule}_#{name}_#{unique_id}.txt", "wb") do |file|
+      file.write(encrypted_data)
+    end
+    self.load_data
+  end
+
+  def self.delete_data(unique_id)
+    teams = Dir.glob("#{BATTLE_LOADER_PATH}/*.txt")
+    file = nil
+    teams.each do |info|
+      file = info if info.include?(unique_id)
+      break
+    end
+    File.delete(file) if file
+    self.load_data
+  end
+
+  def self.add_trainer_data(battle)
+    if pbConfirmMessage(_INTL("Do you want to add the opponent team into Battle Loader?"))
+      self.load_data
+      rules = [_INTL("1v1"), _INTL("2v2"), _INTL("1v2"), _INTL("2v1")]
+      ret = pbMessage(_INTL("Which battle rule do you want?"), rules, 0)
+      if ret >= 0
+        team = battle.pbParty(1)
+        team.each { |pkmn| pkmn.heal }
+        if pbConfirmMessage(_INTL("Would you like to give it a name?"))
+          name = pbEnterText(_INTL("What name?"), 0, 30)
+          self.add_data(rules[ret], name, team)
+        else
+          self.add_data(rules[ret], battle.opponent.sample.name, team)
+        end
+        pbMessage(_INTL("The team registered!"))
       end
     end
   end
 
   def self.open_battle_loader
-    choose = pbMessage(_INTL("What do you want to do?"), [_INTL("Battle"), _INTL("Export Team"), _INTL("Cancel")], -1)
+    choice = [_INTL("Battle"), _INTL("Export Team"), _INTL("Delete Team"), _INTL("Cancel")]
+    choose = pbMessage(_INTL("What do you want to do?"), choice, -1)
     case choose
     when 0
-      if !@@battle_loader.empty?
-        teams_names = []
-        teams = []
-        rules = []
-        names = []
-        @@battle_loader.each do |trainer, battle_info|
-          teams_names.push("#{trainer.split("_")[0]} #{battle_info[1]}")
-          teams.push(battle_info[0])
-          rules.push(battle_info[1])
-          names.push("#{trainer.split("_")[0]}")
-        end
-        index = pbMessage(_INTL("Which team do you want to challenge?"), teams_names, -1)
+      self.load_data
+      if @@battle_loader.empty?
+        pbMessage(_INTL("There isn't any teams in Battle Loader!"))
+      else
+        names = @@battle_loader.map { |team_info| "#{team_info[0]} #{team_info[1]}" }
+        index = pbMessage(_INTL("Which team do you want to challenge?"), names, -1)
         if index >= 0
-          team = teams[index]
-          rule = rules[index]
-          name = names[index]
-          self.start_battle(name, team, rule)
+          rule = @@battle_loader[index][0]
+          team = @@battle_loader[index][2]
+          self.start_battle(rule, team)
         end
       end
     when 1
+      self.load_data
       rules = [_INTL("1v1"), _INTL("2v2"), _INTL("1v2"), _INTL("2v1")]
       ret = pbMessage(_INTL("Which battle rule do you want?"), rules, -1)
       if ret >= 0
-        self.save_data(rules[ret])
+        if pbConfirmMessage(_INTL("Would you like to give it a name?"))
+          name = pbEnterText(_INTL("What name?"), 0, 30)
+          self.add_data(rules[ret], name)
+        else
+          self.add_data(rules[ret])
+        end
         pbMessage(_INTL("Your team exported!"))
+      end
+    when 2
+      self.load_data
+      if @@battle_loader.empty?
+        pbMessage(_INTL("There isn't any teams in Battle Loader!"))
+      else
+        names = @@battle_loader.map { |team_info| "#{team_info[0]} #{team_info[1]}" }
+        index = pbMessage(_INTL("Which team do you want to delete?"), names, -1)
+        unique_id = @@battle_loader[index][3]
+        if index >= 0
+          self.delete_data(unique_id)
+          pbMessage(_INTL("This team has been deleted!"))
+        end
       end
     end
   end
 
-  def self.start_battle(name, team, rule)
+  def self.start_battle(rule, team)
     setBattleRule(rule)
-    $team = team
-    $name = name
-    pbTrainerBattle(:LASS2, "Sienna", nil, false, 0, true)
+    $Trainer.set_ta(:battle_loader, true)
+    name_list = rand(1) == 0 ? BOY_NAMES : GIRL_NAMES
+    $Trainer.set_ta(:name, name_list.sample)
+    $Trainer.set_ta(:team, team)
+    trainer_type = GameData::TrainerType.keys.sample
+    trainer_name = nil
+    GameData::Trainer.each { |trainer| trainer_name = trainer.name if trainer.trainer_type == trainer_type }
+    pbTrainerBattle(trainer_type, trainer_name, nil, false, 0, true)
+    $Trainer.set_ta(:battle_loader, false)
   end
 end
-
-BattleLoader.load_data
