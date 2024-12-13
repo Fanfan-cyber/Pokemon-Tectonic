e04c753f71842c0cc565aec9ce2ea2bbb998468e
Fanfan <erilanfun@gmail.com>
2024-12-13 15:27:05 +0800

    update

--- Plugins/-- fanfan_MOD --/003_Hot_Test.rb
+++ Plugins/-- fanfan_MOD --/003_Hot_Test.rb
@@ -7,31 +7,31 @@ module Input
     hot_test_update
     if triggerex?(:H) && $DEBUG
       HotTest.load_refresh
-    elsif triggerex?(:L) && $DEBUG
-      pbMapInterpreter.execute_script("HotTest.load_refresh")
+    #elsif triggerex?(:L) && $DEBUG
+      #pbMapInterpreter.execute_script("HotTest.load_refresh")
     end
   end
 end
 
 module HotTest
+  FILENAME = './Plugins/-- fanfan_MOD --/998_Test_Content.rb'
   def self.load_refresh
-    filename = './Plugins/-- fanfan_MOD --/998_Hot_Test.rb'
     begin
-      load filename
+      load FILENAME
       pbMessage(_INTL("Reloaded successfully!"))
     rescue LoadError
       begin
-        File.open(filename, 'wb') do |file|
+        File.open(FILENAME, 'wb') do |file|
           file.write("# This is a newly created file.\r\n")
         end
-        load filename
+        load FILENAME
         pbMessage(_INTL("File was not found, but it has been created and loaded."))
-      rescue StandardError
-        pbMessage(_INTL("Failed to create or load the file."))
+      rescue StandardError => e
+        pbMessage(_INTL("Failed to create or load the file.\n({1})", e.message))
       ensure
       end
-    rescue StandardError
-      pbMessage(_INTL("Failed to reload: An error occurred."))
+    rescue StandardError => e
+      pbMessage(_INTL("Failed to reload: An error occurred.\n({1})", e.message))
     ensure
     end
   end
