4b9cf5b5df916b639fdddc2bfd70b74367d8ad6c
Fanfan-cyber <erilanfun@gmail.com>
2025-01-24 13:31:38 +0800

    update v0.1.5 14

--- Plugins/Tectonic Multiple Save/PokemonLoadScreen.rb
+++ Plugins/Tectonic Multiple Save/PokemonLoadScreen.rb
@@ -93,6 +93,7 @@ class PokemonLoadScreen
         cmd_load_game       = -1
         cmd_new_game        = -1
         cmd_achievements    = -1
+        cmd_check_update    = -1
         cmd_debug           = -1
         cmd_website         = -1
         cmd_discord         = -1
@@ -105,6 +106,7 @@ class PokemonLoadScreen
         end
         commands[cmd_new_game = commands.length]        = _INTL("New Game")
         commands[cmd_achievements = commands.length]    = _INTL("Achievements")
+        commands[cmd_check_update = commands.length]    = _INTL("Check Update")
         commands[cmd_website = commands.length]         = _INTL("Website")
         commands[cmd_discord = commands.length]         = _INTL("Discord")
         commands[cmd_wiki = commands.length]            = _INTL("Wiki")
@@ -142,6 +144,10 @@ class PokemonLoadScreen
                     achievementsListScene = AchievementsListScene.new
                     screen = AchievementsListScreen.new(achievementsListScene)
                     screen.pbStartScreen
+                end
+            when cmd_check_update
+                if pbConfirmMessage(_INTL("Current version is {1}.\nCheck update?", MOD_VERSION))
+                  System.launch(AntiAbuse::OFFICIAL_SITE)
                 end
             when cmd_website
                 System.launch("https://www.tectonic-game.com/")
